33ca273b296abc7d85a142d5056cbf5f7acf2351
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-05-21 10:41:03 -0700

    initial files

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+
+const { notes } = require('./db/db');
+
+
+
+
+
+
+
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+
+
+app.listen(3001, () => {
+  console.log(`API server now at port 3001!`);
+});
