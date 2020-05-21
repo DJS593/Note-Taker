182211fcc07656396ba708e9ac0e812da41e5ce1
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-05-21 14:24:44 -0700

    added POST request

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
 // to communicate with Heroku we need to tell the app to communicate with the following environment variable
 const PORT = process.env.PORT || 3001;
 
 // the two codes below are needed to run express
 const express = require('express');
 const app = express();
+
+/* the following is needed so the server recognizes the incoming POST as JSON */
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 
@@ -25,6 +34,41 @@ function filterByQuery(query, notesArray) {
   return filteredResults;
 }
 
+
+function findById(id, notesArray) {
+  const result = notesArray.filter(note => note.id === id)[0];
+  return result;
+}
+
+
+
+
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  
+  return note;
+}
+
+function validateNote(note) {
+  if (!note.title || typeof note.title !== 'string') {
+    return false;
+  }
+  if (!animal.text || typeof animal.text !== 'string') {
+    return false;
+  }
+  if (!animal.id || typeof animal.id !== 'string') {
+    return false;
+  }
+  return true;
+}
+
+
+
 app.get('/api/notes', (req, res) => {
   let results = notes;
   if (req.query) {
@@ -33,9 +77,28 @@ app.get('/api/notes', (req, res) => {
   res.json(results);
 });
 
+app.get('/api/notes/:id', (req, res) => {
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
+});
 
 
+app.post('/api/animals', (req, res) => {
+  // set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
 
+  // if any data in req.body is incorrect, send 400 error back
+  if (!vadidateNote(req.body)) {
+    res.status(400).send('The note is not properly formatted');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+});
 
 
 // the .listen() method allows the server to "listen" for the requests
