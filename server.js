19f204eeb7fb970fd94bb8c77351bdcc80c0fe8a
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-05-21 11:07:52 -0700

    Add Heroku

--- server.js
+++ server.js
@@ -6,14 +6,35 @@ const { notes } = require('./db/db');
 
 
 
-
-
-
-
+function filterByQuery(query, notesArray) {
+  let filteredResults = notesArray;
+  if (query.title) {
+    filteredResults = filteredResults.filter(note => note.title === query.title);
+  }
+  if (query.text) {
+    filteredResults = filteredResults.filter(note => note.text === query.text);
+  }
+  if (query.id) {
+    filteredResults = filteredResults.filter(note => note.id === query.id);
+  }
+  return filteredResults;
+}
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
 });
+
+
+
+
+
+// app.get('/api/notes', (req, res) => {
+//   res.json(notes);
+// });
 
 
 
