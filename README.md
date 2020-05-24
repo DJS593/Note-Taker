f517002bc07022c154271d25390e1d6b061e9a86
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-05-24 13:19:31 -0700

    upon delete the remaining object IDs are re-ordered to eliminate duplicate IDs

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
   [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
   ## Description 
-  The app allows the user to enter notes beginning with a title and then a brief description.  The user can save the note using the save icon.  Once saved, the note will reside on the left side of the page.  In addition, to making new notes, the user can delete unwanted notes using the trash icon.  On the back-end the notes are added / deleted from a JSON file.  Each entry (note) is given a unique ID.
+  The app allows the user to enter notes beginning with a title and then a brief description.  The user can save the note using the save icon.  Once saved, the note will reside on the left side of the page.  In addition, to making new notes, the user can delete unwanted notes using the trash icon.  On the back-end the notes are added / deleted from a JSON file.  Each entry (note) is given a unique ID.  If a note is deleted, the IDs are "re-ordered." 
   
   ## Table of Contents
   * [Installation](#installation)
@@ -11,10 +11,11 @@
   * [Contributing](#contributing)
   * [Tests](#tests) 
   * [Questions](#questions)
+  * [Deployed Application on Heroku](#deployed)
   * [Example](#example)
   
   ## Installation Instructions <a name="installation"></a> 
-  A user will need to clone the repository "Note-Taker", run "npm install express" and sign-up for Heroku if they wish to deploy on the platform.
+  A user will need to clone the repository "Note-Taker", run "npm install express" and sign-up for Heroku if they wish to deploy on the platform.  A platform similar to Heroku is recommended to support the various API calls.  
   
   ## Usage Instructions <a name="usage"></a>
   For any user who wishes to use a convenient tool for taking and storing notes.
@@ -33,5 +34,8 @@
   
   Email stadj099@yahoo.com with any additional questions. 
 
+  ## Deployed Note Taker application on Heroku<a name="deployed"><a>
+  https://note-taker593.herokuapp.com/
+  
   ## Example Note-Taker.html<a name="example"></a>
   ![image](https://user-images.githubusercontent.com/61851131/82748635-97578680-9d58-11ea-9019-3aceed4abede.png)
--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-//require('dotenv').config()
 
 // to communicate with Heroku we need to tell the app to communicate with the following environment variable
 const PORT = process.env.PORT || 3001;
 
-// the two codes below are needed to run express
-
+// set up express app
 const app = express();
 
-/* the following is needed so the server recognizes the incoming POST as JSON */
-// parse incoming string or array data
+// data parsing
 app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
 app.use(express.json());
 app.use(express.static('public'));
-
 
 
 const { notes } = require('./db/db');
 
 
+// routes
 
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+
+// filtering the requested data by title, text or id
 function filterByQuery(query, notesArray) {
   let filteredResults = notesArray;
   if (query.title) {
@@ -37,13 +45,13 @@ function filterByQuery(query, notesArray) {
   return filteredResults;
 }
 
-
+// function is designed to filter by id
 function findById(id, notesArray) {
   const result = notesArray.filter(note => note.id === id)[0];
   return result;
 }
 
-
+// function takes a new note, pushes the note into the notesArray and write a new JSON file
 function createNewNote(body, notesArray) {
   const note = body;
   notesArray.push(note);
@@ -55,18 +63,8 @@ function createNewNote(body, notesArray) {
   return note;
 }
 
-// function deleteNote(body, notesArray) {
-//   const note = body;
-//   notesArray.push(note);
-//   fs.writeFileSync(
-//     path.join(__dirname, './db/db.json'),
-//     JSON.stringify({ notes: notesArray }, null, 2)
-//   );
 
-//   return note;
-// }
-
-
+// validating that the typeof data is expected; string in this app
 function validateNote(note) {
   if (!note.title || typeof note.title !== 'string') {
     return false;
@@ -80,8 +78,20 @@ function validateNote(note) {
   return true;
 }
 
+// routes
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
 
+// display by ID; not relevant to the assignment, but may prove useful in the future
 app.get('/api/notes/:id', (req, res) => {
   const result = findById(req.params.id, notes);
   if (result) {
@@ -91,6 +101,8 @@ app.get('/api/notes/:id', (req, res) => {
   }
 });
 
+
+// displays notes
 app.get('/api/notes', (req, res) => {
   let results = notes;
   if (req.query) {
@@ -100,8 +112,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 
-
-
+// add a note to the db.json file utilizing the app.post method as well as createNewNote
 app.post('/api/notes', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = notes.length.toString();
@@ -116,8 +127,7 @@ app.post('/api/notes', (req, res) => {
 });
 
 
-
-
+// remove a note via ID and then write a new file
 app.delete("/api/notes/:id", function (req, res) {
   var id = req.params.id;
   fs.readFileSync('./db/db.json', (err, data) => {
@@ -128,31 +138,25 @@ app.delete("/api/notes/:id", function (req, res) {
   for (var i = 0; i < notes.length; i++) {
 
       if (notes[i].id == id) {
-          console.log("Deleting ==============");
-          console.log(notes[i]);
-          notes.splice(i, 1);
-      }
+          notes.splice(i, 1);   
+
+          for (var i = 0; i < notes.length; i++) {
+            notes[i].id = i.toString();  
+          }
+          break;
+        }
+  
+  //for (var i = 0; i < notes.length; i++) {
+    //  notes[i].id = i.toString();
+ // }
   }
 
-  fs.writeFile('./db/db.json', JSON.stringify({ notes: notes}, null, 2), (err) => {
+  fs.writeFile('./db/db.json', JSON.stringify({ notes: notes }, null, 2), (err) => {
       if (err) throw err;
-      console.log('The file was updated!');
   });
 
   res.json(id);
 })
-
-
-
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
-});
-
 
 
 // the .listen() method allows the server to "listen" for the requests
